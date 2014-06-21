c5918264989c0754634c8fda6660d8295bc0b1ef
zick <zickminoh@gmail.com>
2014-06-21 13:52:53 +0900

    Add readList and printObj.

--- HaxeLisp.hx
+++ HaxeLisp.hx
@@ -3,9 +3,18 @@ enum LObj {
   Num(num : Int);
   Sym(name : String);
   Error(msg : String);
-  Cons(car : LObj, cdr : LObj);
+  Cons(cell : Cell);
   Subr(fn : LObj -> LObj);
   Expr(args : LObj, body : LObj, env : LObj);
+}
+
+class Cell {
+  public function new (a, d) {
+    this.car = a;
+    this.cdr = d;
+  }
+  public var car : LObj;
+  public var cdr : LObj;
 }
 
 class Util {
@@ -20,16 +29,36 @@ class Util {
 
   public static function safeCar(obj) {
     return switch (obj) {
-      case Cons(car, cdr): car;
+      case Cons(cell): cell.car;
       default: kNil;
     }
   }
 
   public static function safeCdr(obj) {
     return switch (obj) {
-      case Cons(car, cdr): cdr;
+      case Cons(cell): cell.cdr;
       default: kNil;
     }
+  }
+
+  public static function makeCons(a, d) {
+    return Cons(new Cell(a, d));
+  }
+
+  public static function nreverse(lst) {
+    var ret = kNil;
+    while (true) {
+      var cell;
+      switch (lst) {
+        case Cons(c): cell = c;
+        default: break;  // break from while
+      }
+      var tmp = cell.cdr;
+      cell.cdr = ret;
+      ret = lst;
+      lst = tmp;
+    }
+    return ret;
   }
 
   public static var kNil = Nil;
@@ -98,16 +127,76 @@ class Reader {
     } else if (str.charAt(0) == kRPar) {
       return parseError("invalid syntax: " + str);
     } else if (str.charAt(0) == kLPar) {
-      return parseError("noimpl");
+      return readList(str.substring(1));
     } else if (str.charAt(0) == kQuote) {
-      return parseError("noimpl");
+      var tmp = read(str.substring(1));
+      return new ParseState(Util.makeCons(Util.makeSym("quote"),
+                                         Util.makeCons(tmp.obj, Util.kNil)),
+                           tmp.next);
     }
     return readAtom(str);
+  }
+
+  public static function readList(str : String) {
+    var ret = Util.kNil;
+    while (true) {
+      str = skipSpaces(str);
+      if (str.length == 0) {
+        return parseError("unfinished parenthesis");
+      } else if (str.charAt(0) == kRPar) {
+        break;
+      }
+      var tmp = read(str);
+      switch (tmp.obj) {
+        case Error(_): return tmp;  // return from readList
+        default: true;
+      }
+      ret = Util.makeCons(tmp.obj, ret);
+      str = tmp.next;
+    }
+    return new ParseState(Util.nreverse(ret), str.substring(1));
   }
 
   private static var kLPar = "(";
   private static var kRPar = ")";
   private static var kQuote = "'";
+}
+
+class Printer {
+  public static function print(obj) {
+    return switch (obj) {
+      case Nil: "nil";
+      case Num(num): Std.string(num);
+      case Sym(name): name;
+      case Error(msg): "<error: " + msg + ">";
+      case Cons(cell): printList(obj);
+      case Subr(fn): "<subr>";
+      case Expr(args, body, env): "<expr>";
+      default: "<unknown>";
+    }
+  }
+
+  private static function printList(obj) {
+    var ret = "";
+    var first = true;
+    while (true) {
+      switch (obj) {
+        case Cons(_): true;
+        default: break;  // break from while
+      }
+      if (first) {
+        first = false;
+      } else {
+        ret += " ";
+      }
+      ret += print(Util.safeCar(obj));
+      obj = Util.safeCdr(obj);
+    }
+    if (obj == Util.kNil) {
+      return "(" + ret + ")";
+    }
+    return "(" + ret + " . " + print(obj) + ")";
+  }
 }
 
 class HaxeLisp {
@@ -116,7 +205,7 @@ class HaxeLisp {
     Sys.print("> ");
     try while (true) {
       var line = stdin.readLine();
-      Sys.println(Reader.read(line));
+      Sys.println(Printer.print(Reader.read(line).obj));
       Sys.print("> ");
     } catch(_ : haxe.io.Eof) true;
   }
